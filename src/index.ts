736e59715d86e3de15307bddc9f6a9dca791adbd
Fernando Maia <fermaiasoares@aol.com>
2021-03-28 16:47:03 -0300

    Documentacao com Swagger

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import swagger from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerSetup from "./swagger.json";
 
 const app = express();
-
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerSetup));
 app.use(express.json());
 
 app.use("/users", usersRoutes);
